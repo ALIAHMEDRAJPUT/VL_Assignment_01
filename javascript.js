047173422292772837eaac10a55e1ce5308fcb9f
Ali Ahmad <786aliahmedrajput@gmail.com>
2022-10-30 14:06:55 +0500

    changing AB#64

--- javascript.js
+++ javascript.js
@@ -6,3 +6,12 @@
   -----
 </script>
 <script id="tid2" type="text/template">
+  
+  <script id="id1" type="text/template">
+  -----
+</script>
+<script id="tid2" type="text/template">
+<script id="id1" type="text/template">
+  -----
+</script>
+<script id="tid2" type="text/template">
