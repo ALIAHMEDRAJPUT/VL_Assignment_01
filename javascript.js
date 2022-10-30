5c1bbeb98130034a01c3229768b6701c34555698
Ali Ahmad <786aliahmedrajput@gmail.com>
2022-10-30 14:05:56 +0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,3 +6,11 @@
   -----
 </script>
 <script id="tid2" type="text/template">
+  <script id="id1" type="text/template">
+  -----
+</script>
+<script id="tid2" type="text/template">
+<script id="id1" type="text/template">
+  -----
+</script>
+<script id="tid2" type="text/template">
